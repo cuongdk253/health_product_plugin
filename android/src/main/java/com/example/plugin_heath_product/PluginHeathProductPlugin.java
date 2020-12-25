c8211a8d03dcd04e5835aaf7f459232c735e58ae
vietvuong1994 <vietvuong1994@gmail.com>
2020-12-25 21:49:32 +0700

    test android done

--- android/src/main/java/com/example/plugin_heath_product/PluginHeathProductPlugin.java
+++ android/src/main/java/com/example/plugin_heath_product/PluginHeathProductPlugin.java
@@ -3,8 +3,8 @@ package com.example.plugin_heath_product;
 import android.app.Activity;
 
 import androidx.annotation.NonNull;
-
 import com.example.mytoast.MyToast;
+
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
@@ -14,7 +14,7 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
-/** FlutterToastPlugin */
+/** PluginHeathProductPlugin */
 public class PluginHeathProductPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
@@ -22,12 +22,6 @@ public class PluginHeathProductPlugin implements FlutterPlugin, MethodCallHandle
   /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
   private Activity activity;
-
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_toast");
-    channel.setMethodCallHandler(this);
-  }
 
   @Override
   public void onAttachedToActivity(ActivityPluginBinding binding) {
@@ -44,18 +38,18 @@ public class PluginHeathProductPlugin implements FlutterPlugin, MethodCallHandle
   public void onDetachedFromActivity() {}
 
   @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "plugin_heath_product");
+    channel.setMethodCallHandler(this);
+  }
+
+  @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     if (call.method.equals("getPlatformVersion")) {
       result.success("Android " + android.os.Build.VERSION.RELEASE);
-    }
-    else if(call.method.equals("showToast")){
-      String msg = call.argument("msg").toString();
-      MyToast.showToast(activity, msg);
-    }
-    else if(call.method.equals("openPage")){
+    } else if(call.method.equals("openPage")){
       MyToast.showActivity(activity);
-    }
-    else {
+    } else {
       result.notImplemented();
     }
   }
