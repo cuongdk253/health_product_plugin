d7747c2a7ee43f73ec03cff77bfe41c3d4ab2d8d
cuongdk253 <cuongdk253@gmail.com>
2020-12-28 08:16:03 +0700

    pull

--- ios/Classes/SwiftPluginHeathProductPlugin.swift
+++ ios/Classes/SwiftPluginHeathProductPlugin.swift
@@ -17,36 +17,19 @@ public class SwiftPluginHeathProductPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    // result("iOS " + UIDevice.current.systemVersion)
     if (call.method == "getPlatformVersion") {
       result("iOS " + UIDevice.current.systemVersion)
     } else if (call.method == "openPage") {
         
         let oldViewController = UIApplication.shared.keyWindow?.rootViewController as? FlutterViewController
         
-        let navigationController = UINavigationController(rootViewController: HomeViewController())
-//        oldViewController?.present(navigationController, animated: true)
+        let navigationController = UINavigationController(rootViewController: PageCtrlll())
         navigationController.modalPresentationStyle = .fullScreen
         navigationController.transitioningDelegate = transitionDelegate
         
         oldViewController?.present(navigationController, animated: true, completion: nil)
-        
-//        print(oldViewController)
-//
-//        let homePage = HomeViewController()
-//        homePage.oldNavigator = oldViewController
-//
-//        let navigationController = UINavigationController(rootViewController: homePage)
-//        let window = UIApplication.shared.keyWindow
-//        window?.rootViewController = navigationController
-//        window?.subviews.last?.backgroundColor = .orange
-
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
-//            window?.rootViewController = oldViewController
-//        }
-        
-        
-        result("iOS " + UIDevice.current.systemVersion)
+                
+        result(1)
     }
   }
     
